cd73ad1c58aa8148d20c72781e49df7c11998048
CarlossRodriguez8 <carlosrodriguez8ra@gmail.com>
2026-05-05 20:10:54 -0300

    Create chat.controller.js

---
+++ src/controllers/chat.controller.js
@@ -0,0 +1,27 @@
+import { Chat } from "../models/chat.model";
+
+const getChats = async (req, res) => {
+    const chats = await Chat.find();
+    res.json(chats);
+}
+
+const addChat = async (req, res) => {
+    const body = req.body;
+    await Chat.create({});
+    res.json({ status: 'Chat agregado' });
+}
+
+const searchChat = async (req, res) => {
+    const id = req.params.id;
+    const chat = await Chat.findById(id);
+    if (!chat) return res.status(404).send('Chat no encontrado');
+    res.json(chat);
+}
+
+const deleteChat = async (req, res) => {
+    const id = req.params.id;
+    await Chat.findByIdAndDelete(id);
+    res.json({ status: 'Chat eliminado' });
+}
+
+export { getChats, addChat, searchChat, deleteChat }
