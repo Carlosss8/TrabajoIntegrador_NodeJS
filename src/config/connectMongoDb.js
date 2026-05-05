8a7ee975cbe47b6b69007edf6ce6417e7e464bc1
CarlossRodriguez8 <carlosrodriguez8ra@gmail.com>
2026-05-05 20:10:39 -0300

    update connectDB

--- src/config/connectMongoDb.js
+++ src/config/connectMongoDb.js
@@ -1,9 +1,14 @@
+import "dotenv/config";
 import mongoose from "mongoose"
+
 const connectMongoDb = async () => {
     try {
+        await mongoose.connect(process.env.MONGO_URI)
+        console.log('Conexión a la DB establecida');
 
     } catch (error) {
-
+        console.log(error.message)
+        console.error('Error al conectar a la DB:', error);
     }
 }
 
