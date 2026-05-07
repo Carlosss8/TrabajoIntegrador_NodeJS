6b3cda19196ae1c2be66dfe38c8ddaf70652612f
CarlossRodriguez8 <carlosrodriguez8ra@gmail.com>
2026-05-06 22:09:14 -0300

    Create error.middleware.js

---
+++ src/middlewares/error.middleware.js
@@ -0,0 +1,11 @@
+const errorMiddleware = (err, req, res, next) => {
+    console.error(err);
+
+    res.status(500).json({
+        success: false,
+        data: null,
+        message: "Error interno del servidor"
+    });
+};
+
+export { errorMiddleware };
