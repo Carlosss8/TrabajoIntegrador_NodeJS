a8e51aac6f6b8fc138182a5c822169df21f748bc
CarlossRodriguez8 <carlosrodriguez8ra@gmail.com>
2026-05-07 20:50:36 -0300

    fix error handling and responses

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,41 +1,110 @@
 import { User } from "../models/user.model.js";
 
-const getUsers = async (req, res) => {
-    const users = await User.find();
-    res.json(users);
-}
+const getUsers = async (req, res, next) => {
+    try {
+        const users = await User.find();
 
-const addUser = async (req, res) => {
-    const newUser = await User.create({
-        name: body.name,
-        email: body.email
-    });
+        res.status(200).json({
+            success: true,
+            data: users,
+            message: "Usuarios obtenidos correctamente"
+        });
+    } catch (error) {
+        next(error);
+    }
+};
 
-    res.status(201).json({
-        success: true,
-        data: newUser,
-        message: "Usuario agregado correctamente"
-    });
-}
+const addUser = async (req, res, next) => {
+    try {
+        const body = req.body;
 
-const searchUser = async (req, res) => {
-    const id = req.params.id;
-    const user = await User.findById(id);
-    if (!user) return res.status(404).send('Usuario no encontrado');
-    res.json(user);
-}
+        const newUser = await User.create({
+            name: body.name,
+            email: body.email
+        });
 
-const updateUser = async (req, res) => {
-    const id = req.params.id;
-    const body = req.body;
-    const update = await User.findByIdAndUpdate(id, body, { new: true, runValidators: true });
-    res.json({ status: 'Usuario actualizado', update });
-}
+        res.status(201).json({
+            success: true,
+            data: newUser,
+            message: "Usuario agregado correctamente"
+        });
+    } catch (error) {
+        next(error);
+    }
+};
 
-const deleteUser = async (req, res) => {
-    const id = req.params.id;
-    await User.findByIdAndDelete(id);
-    res.json({ status: 'Usuario eliminado' });
-}
+const searchUser = async (req, res, next) => {
+    try {
+        const id = req.params.id;
+        const user = await User.findById(id);
 
-export { getUsers, addUser, searchUser, updateUser, deleteUser }
+        if (!user) {
+            return res.status(404).json({
+                success: false,
+                data: null,
+                message: "Usuario no encontrado"
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            data: user,
+            message: "Usuario encontrado correctamente"
+        });
+    } catch (error) {
+        next(error);
+    }
+};
+
+const updateUser = async (req, res, next) => {
+    try {
+        const id = req.params.id;
+        const body = req.body;
+
+        const update = await User.findByIdAndUpdate(id, body, {
+            new: true,
+            runValidators: true
+        });
+
+        if (!update) {
+            return res.status(404).json({
+                success: false,
+                data: null,
+                message: "Usuario no encontrado"
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            data: update,
+            message: "Usuario actualizado correctamente"
+        });
+    } catch (error) {
+        next(error);
+    }
+};
+
+const deleteUser = async (req, res, next) => {
+    try {
+        const id = req.params.id;
+        const deletedUser = await User.findByIdAndDelete(id);
+
+        if (!deletedUser) {
+            return res.status(404).json({
+                success: false,
+                data: null,
+                message: "Usuario no encontrado"
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            data: deletedUser,
+            message: "Usuario eliminado correctamente"
+        });
+    } catch (error) {
+        next(error);
+    }
+};
+
+export { getUsers, addUser, searchUser, updateUser, deleteUser };
