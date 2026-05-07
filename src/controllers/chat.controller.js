181a1e56f03e3fc3566eb6b61a0e67c6185f5ef1
CarlossRodriguez8 <carlosrodriguez8ra@gmail.com>
2026-05-07 20:50:31 -0300

    fix error handling and responses

--- src/controllers/chat.controller.js
+++ src/controllers/chat.controller.js
@@ -1,27 +1,80 @@
 import { Chat } from "../models/chat.model.js";
 
-const getChats = async (req, res) => {
-    const chats = await Chat.find();
-    res.json(chats);
-}
+const getChats = async (req, res, next) => {
+    try {
+        const chats = await Chat.find();
 
-const addChat = async (req, res) => {
-    const body = req.body;
-    await Chat.create({ title: body.title });
-    res.json({ status: 'Chat agregado' });
-}
+        res.status(200).json({
+            success: true,
+            data: chats,
+            message: "Chats obtenidos correctamente"
+        });
+    } catch (error) {
+        next(error);
+    }
+};
 
-const searchChat = async (req, res) => {
-    const id = req.params.id;
-    const chat = await Chat.findById(id);
-    if (!chat) return res.status(404).send('Chat no encontrado');
-    res.json(chat);
-}
+const addChat = async (req, res, next) => {
+    try {
+        const body = req.body;
+        const newChat = await Chat.create({
+            title: body.title
+        });
 
-const deleteChat = async (req, res) => {
-    const id = req.params.id;
-    await Chat.findByIdAndDelete(id);
-    res.json({ status: 'Chat eliminado' });
-}
+        res.status(201).json({
+            success: true,
+            data: newChat,
+            message: "Chat agregado correctamente"
+        });
+    } catch (error) {
+        next(error);
+    }
+};
 
-export { getChats, addChat, searchChat, deleteChat }
+const searchChat = async (req, res, next) => {
+    try {
+        const id = req.params.id;
+        const chat = await Chat.findById(id);
+
+        if (!chat) {
+            return res.status(404).json({
+                success: false,
+                data: null,
+                message: "Chat no encontrado"
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            data: chat,
+            message: "Chat encontrado correctamente"
+        });
+    } catch (error) {
+        next(error);
+    }
+};
+
+const deleteChat = async (req, res, next) => {
+    try {
+        const id = req.params.id;
+        const deletedChat = await Chat.findByIdAndDelete(id);
+
+        if (!deletedChat) {
+            return res.status(404).json({
+                success: false,
+                data: null,
+                message: "Chat no encontrado"
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            data: deletedChat,
+            message: "Chat eliminado correctamente"
+        });
+    } catch (error) {
+        next(error);
+    }
+};
+
+export { getChats, addChat, searchChat, deleteChat };
--- src/controllers/messages.controller.js
+++ src/controllers/messages.controller.js
@@ -1,40 +1,108 @@
 import { Messages } from "../models/messages.model.js";
 
-const getMessages = async (req, res) => {
-    const messages = await Messages.find()
-        .populate("userId")
-        .populate("chatId");
+const getMessages = async (req, res, next) => {
+    try {
+        const messages = await Messages.find()
+            .populate("userId")
+            .populate("chatId");
 
-    res.json(messages);
-}
+        res.status(200).json({
+            success: true,
+            data: messages,
+            message: "Mensajes obtenidos correctamente"
+        });
+    } catch (error) {
+        next(error);
+    }
+};
 
-const getMessagesByChat = async (req, res) => {
-    const chatId = req.params.chatId;
+const getMessagesByChat = async (req, res, next) => {
+    try {
+        const chatId = req.params.chatId;
 
-    const messages = await Messages.find({ chatId })
-        .populate("userId")
-        .populate("chatId");
+        const messages = await Messages.find({ chatId })
+            .populate("userId")
+            .populate("chatId");
 
-    res.json(messages);
-}
+        res.status(200).json({
+            success: true,
+            data: messages,
+            message: "Historial de mensajes obtenido correctamente"
+        });
+    } catch (error) {
+        next(error);
+    }
+};
 
-const addMessages = async (req, res) => {
-    const body = req.body;
-    await Messages.create({ message: body.message, chatId: body.chatId, userId: body.userId });
-    res.json({ status: 'Mensaje enviado' });
-}
+const addMessages = async (req, res, next) => {
+    try {
+        const body = req.body;
 
-const updateMessages = async (req, res) => {
-    const id = req.params.id;
-    const body = req.body;
-    const update = await Messages.findByIdAndUpdate(id, body, { new: true, runValidators: true });
-    res.json({ status: 'Mensaje editado', update });
-}
+        const newMessage = await Messages.create({
+            message: body.message,
+            chatId: body.chatId,
+            userId: body.userId
+        });
 
-const deleteMessages = async (req, res) => {
-    const id = req.params.id;
-    await Messages.findByIdAndDelete(id);
-    res.json({ status: 'Mensaje eliminado' });
-}
+        res.status(201).json({
+            success: true,
+            data: newMessage,
+            message: "Mensaje enviado correctamente"
+        });
+    } catch (error) {
+        next(error);
+    }
+};
 
-export { getMessages, getMessagesByChat, addMessages, updateMessages, deleteMessages }
+const updateMessages = async (req, res, next) => {
+    try {
+        const id = req.params.id;
+        const body = req.body;
+
+        const update = await Messages.findByIdAndUpdate(id, body, {
+            new: true,
+            runValidators: true
+        });
+
+        if (!update) {
+            return res.status(404).json({
+                success: false,
+                data: null,
+                message: "Mensaje no encontrado"
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            data: update,
+            message: "Mensaje editado correctamente"
+        });
+    } catch (error) {
+        next(error);
+    }
+};
+
+const deleteMessages = async (req, res, next) => {
+    try {
+        const id = req.params.id;
+        const deletedMessage = await Messages.findByIdAndDelete(id);
+
+        if (!deletedMessage) {
+            return res.status(404).json({
+                success: false,
+                data: null,
+                message: "Mensaje no encontrado"
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            data: deletedMessage,
+            message: "Mensaje eliminado correctamente"
+        });
+    } catch (error) {
+        next(error);
+    }
+};
+
+export { getMessages, getMessagesByChat, addMessages, updateMessages, deleteMessages };
